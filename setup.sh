801e2308c76542c2f831c19c129e3d78a8b648c5
David Liu <davidas99017@gmail.com>
2025-12-04 11:49:23 -0800

    Syncing script updates

--- setup.sh
+++ setup.sh
@@ -65,12 +65,18 @@ for module_name in "quickshell" "hypr" "matugen" "wezterm"; do
     read -p "$(echo -e "${YELLOW}Moving previous $module_name configuration to $BACKUP_DIR/$module_name? Previous config will be overwritten if not backed up... [Y/n]: ${NC}")" -n 1 -r
     REPLY=${REPLY:-Y}
     if [[ $REPLY =~ ^[Yy]$ ]]; then
+      echo ""
       mkdir -p "$BACKUP_DIR"
       mv "$HOME/.config/$module_name" "$BACKUP_DIR/$module_name"
       print_success "Backup complete: $BACKUP_DIR/$module_name"
       echo ""
+    else
+      echo ""
+      rm -rf "$HOME/.config/$module_name"
     fi
   fi
+
+  echo ""
 
   print_info "Creating symlink for $module_name..."
   ln -s "$SUBMODULE_PATH/config_dots/$module_name" "$HOME/.config/$module_name"
@@ -97,46 +103,46 @@ LOCAL_BIN="$HOME/.local/bin"
 WEZTERM_LINK="$LOCAL_BIN/wezterm"
 
 setup_wezterm_symlink() {
-    mkdir -p "$LOCAL_BIN"
-    [ -e "$WEZTERM_LINK" ] && rm -f "$WEZTERM_LINK"
-    ln -s "$WEZTERM_BINARY" "$WEZTERM_LINK"
-    print_success "WezTerm symlink created: $WEZTERM_LINK"
+  mkdir -p "$LOCAL_BIN"
+  [ -e "$WEZTERM_LINK" ] && rm -f "$WEZTERM_LINK"
+  ln -s "$WEZTERM_BINARY" "$WEZTERM_LINK"
+  print_success "WezTerm symlink created: $WEZTERM_LINK"
 }
 
 if [ -f "$WEZTERM_BINARY" ]; then
-    print_info "WezTerm binary found, skipping build..."
-    if [ ! -L "$WEZTERM_LINK" ] || [ "$(readlink -f "$WEZTERM_LINK")" != "$(readlink -f "$WEZTERM_BINARY")" ]; then
-        setup_wezterm_symlink
-    else
-        print_success "WezTerm symlink already configured correctly"
-    fi
+  print_info "WezTerm binary found, skipping build..."
+  if [ ! -L "$WEZTERM_LINK" ] || [ "$(readlink -f "$WEZTERM_LINK")" != "$(readlink -f "$WEZTERM_BINARY")" ]; then
+    setup_wezterm_symlink
+  else
+    print_success "WezTerm symlink already configured correctly"
+  fi
 else
-    print_info "Building WezTerm from source..."
+  print_info "Building WezTerm from source..."
 
-    command -v rustc &>/dev/null || {
-        print_info "Installing Rust..."
-        curl https://sh.rustup.rs -sSf | sh -s -- -y
-    }
+  command -v rustc &>/dev/null || {
+    print_info "Installing Rust..."
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+  }
 
-    [ ! -d "$WEZTERM_DIR" ] && {
-        print_info "Cloning WezTerm repository..."
-        git clone --depth=1 --branch=main --recursive https://github.com/wez/wezterm.git "$WEZTERM_DIR"
-    }
+  [ ! -d "$WEZTERM_DIR" ] && {
+    print_info "Cloning WezTerm repository..."
+    git clone --depth=1 --branch=main --recursive https://github.com/wez/wezterm.git "$WEZTERM_DIR"
+  }
 
-    cd "$WEZTERM_DIR" || exit 1
-    git submodule update --init --recursive
-    ./get-deps
-    cargo build --release
+  cd "$WEZTERM_DIR" || exit 1
+  git submodule update --init --recursive
+  ./get-deps
+  cargo build --release
 
-    if [ $? -eq 0 ]; then
-        print_success "WezTerm built..."
-        setup_wezterm_symlink
-    else
-        print_error "WezTerm build failed... Exiting..."
-        exit 1
-    fi
+  if [ $? -eq 0 ]; then
+    print_success "WezTerm built..."
+    setup_wezterm_symlink
+  else
+    print_error "WezTerm build failed... Exiting..."
+    exit 1
+  fi
 
-    cd "$SUBMODULE_PATH" || exit 1
+  cd "$SUBMODULE_PATH" || exit 1
 fi
 
 echo ""
